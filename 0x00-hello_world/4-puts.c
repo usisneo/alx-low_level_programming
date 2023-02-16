eb8d654bf5a65002f2c6c3a53ae6cb61b095f543
Neo <neomohlakwana@gmail.com>
2023-02-16 12:26:51 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -program printing a line which puts function
+ * return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
