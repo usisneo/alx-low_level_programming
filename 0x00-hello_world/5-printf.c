4692a27030475ee3fec25ae9de4bd84456df8ff5
Neo <neomohlakwana@gmail.com>
2023-02-16 15:30:52 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that uses printf function to print a line
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
