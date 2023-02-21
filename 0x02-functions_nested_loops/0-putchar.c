403c9713986b190a33e5ab76b8b4ef76ea40e991
Neo <neomohlakwana@gmail.com>
2023-02-21 15:02:23 -0800

    #include "main.h" /**  * Main - main function printing putchar  * description- print _putchar  * Return - 0 always  */
    int main(void)
    {
            _putchar(_);
            _putchar(p);
            _putchar(u);
            _putchar(t);
            _putchar(c);
            _putchar(h);
            _putchar(a);
            _putchar(r);
            _putchar(n);
    git commit -m 0-putchar.c
    clear

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,19 @@
 #include "main.h"
 /**
- * Main - main function printing putchar
+ * Main:  main function printing putchar
  * description- print _putchar
- * Return - 0 always
+ * Return: 0 always
  */
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
-}
+        _putchar('_');
+        _putchar('p');
+        _putchar('u');
+        _putchar('t');
+        _putchar('c');
+        _putchar('h');
+        _putchar('a');
+        _putchar('r');
+        _putchar('\n');
+        return (0);
