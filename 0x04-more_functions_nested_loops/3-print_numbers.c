7515002ae6d44f3928e7c727a8a8ca44fea6fb17
Neo <neomohlakwana@gmail.com>
2023-02-23 11:30:50 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,14 +5,13 @@
  *
  * Return: Always 0.
  */
-
 void print_numbers(void)
 {
-int c;
+ int c;
 
-for (c = 48; c < 58; c++)
-{
-_putchar(c);
-}
-_putchar('\n');
+ for (c = 48; c < 58; c++)
+ {
+ _putchar(c);
+ }
+ _putchar('\n');
 }
