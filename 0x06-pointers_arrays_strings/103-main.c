f8e52d0130a5d09040ff5ae8d1b875add3bb4482
Neo <neomohlakwana@gmail.com>
2023-03-02 08:31:22 -0800

    103-main.c

--- 0x06-pointers_arrays_strings/103-main.c
+++ 0x06-pointers_arrays_strings/103-main.c
@@ -17,43 +17,43 @@ char *res;
 
 res = infinite_add(n, m, r, 100);
 if (res == 0)
-        {
-                printf("Error\n");
-        }
-        else
-        {
-                printf("%s + %s = %s\n", n, m, res);
-        }
-        n = "1234567890";
-        m = "1";
-        res = infinite_add(n, m, r2, 10);
-        if (res == 0)
-        {
-                printf("Error\n");
-        }
-        else
-        {
-                printf("%s + %s = %s\n", n, m, res);
-        }
-        n = "999999999";
-        m = "1";
-        res = infinite_add(n, m, r2, 10);
-        if (res == 0)
-        {
-                printf("Error\n");
-        }
-        else
-        {
-                printf("%s + %s = %s\n", n, m, res);
-        }
-        res = infinite_add(n, m, r3, 11);
-        if (res == 0)
-        {
-                printf("Error\n");
-        }
-        else
-        {
-                printf("%s + %s = %s\n", n, m, res);
-        }
-        return (0);
+{
+printf("Error\n");
+}
+else
+{
+printf("%s + %s = %s\n", n, m, res);
+}
+n = "1234567890";
+m = "1";
+res = infinite_add(n, m, r2, 10);
+if (res == 0)
+{
+printf("Error\n");
+}
+else
+{
+printf("%s + %s = %s\n", n, m, res);
+}
+n = "999999999";
+m = "1";
+res = infinite_add(n, m, r2, 10);
+if (res == 0)
+{
+printf("Error\n");
+}
+else
+{
+printf("%s + %s = %s\n", n, m, res);
+}
+res = infinite_add(n, m, r3, 11);
+if (res == 0)
+{
+printf("Error\n");
+}
+else
+{
+printf("%s + %s = %s\n", n, m, res);
+}
+return (0);
 }
